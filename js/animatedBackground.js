94f79a3dded7f6acb8104e27689f214cc4bd6f1e
SeptoForce <septoforcemc@gmail.com>
2023-01-11 14:54:51 +0100

    Update animatedBackground.js
    changed distance between dots in animatedBackground

--- js/animatedBackground.js
+++ js/animatedBackground.js
@@ -1,10 +1,10 @@
 const body = document.querySelector('body');
 const dotColor = "#ccc";
 body.style.backgroundImage = `radial-gradient(${dotColor} 2px, transparent 2px)`;
-body.style.backgroundSize = '50px 50px';
+body.style.backgroundSize = '40px 40px';
 let scroll = 0;
 setInterval(() => {
     body.style.backgroundPosition = `${scroll}px -${scroll}px`;
     scroll += 0.5;
-    if(scroll > 50) scroll = 0;
+    if(scroll > 40) scroll = 0;
 }, 17);
